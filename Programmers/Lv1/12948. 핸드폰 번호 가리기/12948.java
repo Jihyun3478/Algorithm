77f87a22843ce73eb25219d14c2299aaa62581c5
Jihyun3478 <leejihyun3478@gmail.com>
2023-04-14 13:02:01 +0900

    Update 12948.java
    update

--- Programmers/Lv1/12948. 핸드폰 번호 가리기/12948.java
+++ Programmers/Lv1/12948. 핸드폰 번호 가리기/12948.java
@@ -1,3 +1,15 @@
+/*
+class Solution {
+    public String solution(String phone_number) {
+        String notStar = phone_number.substring(phone_number.length()-4, phone_number.length());
+        String star = phone_number.substring(0, phone_number.length()-4);
+
+        star = star.replaceAll("[0-9]", "*");
+        return star + notStar;
+    }
+}
+*/
+
 class Solution {
     public String solution(String phone_number) {
         char[] ch = phone_number.toCharArray();
