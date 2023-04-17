e3ebfffa850ff69b352b33884c6411a070c2b42d
Jihyun3478 <leejihyun3478@gmail.com>
2023-04-17 20:45:40 +0900

    update 2023.04.17(2)

---
+++ Programmers/Lv1/12930. 이상한 문자 만들기/12930.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String[] strArr = s.split("");
+        String answer = "";
+        int cnt = 0;
+
+        for(String str : strArr) {
+            if(str.contains(" ")) cnt = 0;
+            else cnt++;
+
+            if(cnt % 2 == 0) answer += str.toLowerCase();
+            else answer += str.toUpperCase();
+        }
+        return answer;
+    }
+}
