688b5e972b7fa7dde62973116e355407f8b39c65
Jihyun3478 <leejihyun3478@gmail.com>
2023-05-23 09:58:54 +0900

    update 2023.05.23

--- Baekjoon/스택/10773. 제로/10773.java
+++ Baekjoon/스택/10773. 제로/10773.java
@@ -1,6 +1,3 @@
-/*
- * 2023.05.16
- */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -28,35 +25,6 @@ public class Main {
         int sum = 0;
         for(int i : stack) {
             sum += i;
-        }
-        System.out.println(sum);
-    }
-}
-
-/*
- * 2023.05.21
- */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int K = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < K; i++) {
-            int num = Integer.parseInt(br.readLine());
-
-            if(num != 0) stack.push(num);
-            else stack.pop();
-        }
-
-        int sum = 0;
-        for(int num : stack) {
-            sum += num;
         }
         System.out.println(sum);
     }
