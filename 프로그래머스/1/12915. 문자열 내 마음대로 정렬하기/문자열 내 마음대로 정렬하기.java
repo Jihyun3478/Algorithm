b747b573e203a183ca77f144e69f20b554f2c256
Jihyun3478 <leejihyun3478@gmail.com>
2026-06-09 10:23:55 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 2.97 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -7,7 +7,6 @@ class Solution {
                 return s1.compareTo(s2);
             }
             return s1.charAt(n) - s2.charAt(n);
-            // return Character.compare(s1.charAt(n), s2.charAt(n));
         });
         return strings;
     }
