cb6e0c223ee2a5cd97ace1c60a92d0ec6a4b2cd7
Jihyun3478 <leejihyun3478@gmail.com>
2026-06-15 17:57:59 +0900

    [level 2] Title: 타겟 넘버, Time: 516.42 ms, Memory: 204 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0}); // 1
+        
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            
+            if (temp[1] == numbers.length) {
+                if (temp[0] == target) {
+                    answer++;
+                }
+                continue;
+            }
+            
+            queue.add(new int[]{temp[0] + numbers[temp[1]], temp[1] + 1}); // 1
+            queue.add(new int[]{temp[0] - numbers[temp[1]], temp[1] + 1}); // -1
+        }
+        return answer;
+    }
+}
