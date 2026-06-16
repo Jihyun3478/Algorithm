16d1bfa5e43d1113fde47c52815f94898b845ded
Jihyun3478 <leejihyun3478@gmail.com>
2026-06-16 11:23:15 +0900

    [level 3] Title: 네트워크, Time: 0.56 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,13 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] computers) {
-        //   A B C
-        // A 1 1 0
-        // B 1 1 0
-        // C 0 0 1
-        //    A
-        //  /
-        // B     C
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[computers.length];
         
@@ -17,20 +10,20 @@ class Solution {
             if (visited[i] == true) {
                 continue;
             }
-            visited[i] = true;
             queue.add(i);
+            visited[i] = true;
             
-            networkCnt++;
             while (!queue.isEmpty()) {
                 int temp = queue.poll();
-                
+            
                 for (int j = 0; j < n; j++) {
                     if (computers[temp][j] == 1 && visited[j] == false) {
-                        visited[j] = true;
                         queue.add(j);
+                        visited[j] = true;
                     }
                 }
             }
+            networkCnt++;
         }
         return networkCnt;
     }
